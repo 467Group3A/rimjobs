cab9a5894db42962cbc81632f2eddac7bbb3011f
ryan <ryanpark@usa.com>
2023-03-31 19:57:39 -0500

    Added loadInventory(), it takes part ids from legacyDB giving them an inventory amount from 1-100

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+// File: server.js
+// Date: 3/31/2023
+// Made by: Ryan Park
+// Description: Main driver for node/express server
+//      it will handle all back end interactions with
+//      routing and interacting with the two databases
+//
+////////////////////////////////////////////////
 const express = require("express")
-const app = express()
 const mysql = require("mysql2/promise")
+const sqlite3 = require('sqlite3').verbose()
 const vue = require('vue')
+const app = express()
 const port = 3001
 
 /*
  * New Database establish connection
  *
  * */
-const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./rimjobs.db' , sqlite3.OPEN_READWRITE, (err) => {
     if(err)
     {
@@ -16,7 +24,7 @@ const db = new sqlite3.Database('./rimjobs.db' , sqlite3.OPEN_READWRITE, (err) =
     }
     else
     {
-        console.log('Database connected')
+        console.log('New Database connected')
     }
 })
 
@@ -32,6 +40,60 @@ const legacyPool = mysql.createPool({
     user: 'student',
     password: 'student'
 })
+
+// Function that will take inventory parts
+//   from legacy database, storing the ID
+//    and a random amount of inventory from 1-100
+//    into the new database rimjobs.db
+//
+// ******** I SHOULD REALLY BE DROPPING AND INITALIZING CONNECTIONS WHEN NEED BE *******
+//
+// Known bug right now: I ran the server ~15 times and 1 time it had a problem where the table
+//                      was not created in the newDB
+//
+//***************************************************************** */
+async function loadInventory() {
+    try{
+        // Grab parts from legacy storing in an array
+        const [legacyRows] = await legacyPool.query('SELECT number FROM parts')
+
+        // Drop the table if exists, this is a tactic that will stop ege from bamboozling
+        const dropTableQuery = 'DROP TABLE IF EXISTS parts_inventory'
+        db.run(dropTableQuery,  (err) => {
+            if(err)
+                console.log(err.message)
+        })
+
+        const createTableQuery = 'CREATE TABLE IF NOT EXISTS parts_inventory (number INT, amount INT)'
+        db.run(createTableQuery, [], async (err) => {
+            if (err) {
+                console.log(err)
+                console.log('loadInventory() could not create a new table!')
+                return
+            }
+
+            // Generate and execute INSERT queries for each part ID
+            for (const part of legacyRows) {
+                // Generates random inventory amount from 1-100
+                const randomAmount = Math.floor(Math.random() * 100) + 1;
+                // Insert the part ID(number) and random amount into the parts_inventory table
+                const insertQuery = `INSERT INTO parts_inventory (number, amount) VALUES (${part.number}, ${randomAmount})`
+                db.run(insertQuery, [], (err) => {
+                    if (err) {
+                        console.log(err)
+                        console.log(`loadInventory() could not insert part with ID ${part.number} into rimjobsdb!`)
+                    }
+                });
+            }
+            console.log('loadInventory() inserted randomized inventory to the newdb!')
+        });
+
+        console.log('loadInventory() had no problems!')
+    } catch(err) {
+        console.log(err)
+        console.log('loadInventory() had a problem connecting and querying from the databases!')
+    }
+}
 
 // Middleware
 app.use(express.json())
@@ -56,4 +118,6 @@ app.use(express.static('views'));
 
 app.listen(port , () => {
     console.log(`Running on http://45.33.66.75:${port}`)
+    // Populate inventory in new db using legacy part ids
+    loadInventory()
 })
