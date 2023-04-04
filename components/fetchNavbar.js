3580aabd7622d944e616955c071c12c0f4df056f
David <master4491@gmail.com>
2023-04-04 18:39:28 -0500

    View all Products now points to Ryans /viewinventory. Along with making the styles the same

--- components/fetchNavbar.js
+++ components/fetchNavbar.js
@@ -16,11 +16,11 @@ $(document).ready(function () {
                 <div class="collapse navbar-collapse" id="collapsibleNavId">
                     <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                         <li class="nav-item">
-                            <a id="bouncify" class="nav-link Stillwater fs-5" href="">
+                            <a id="bouncify" class="nav-link Stillwater fs-5" href="/">
                                 <i class="fa-solid fa-warehouse"></i> Home</a>
                         </li>
                         <li class="nav-item">
-                            <a id="spinnify" class="nav-link Stillwater fs-5" href="">
+                            <a id="spinnify" class="nav-link Stillwater fs-5" href="/viewinventory">
                                 <i class="fas fa-dharmachakra"></i> View All Products</a>
                         </li>
                         <li class="nav-item">
