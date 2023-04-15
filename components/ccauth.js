d463fcdf721f0e89e9a8aef65174a68b8acd66f0
David <master4491@gmail.com>
2023-04-15 11:36:33 -0500

    Order Number Generator

--- components/ccauth.js
+++ components/ccauth.js
@@ -1,34 +1,44 @@
 // ccauth.js
 
 const app = Vue.createApp({
-    data() {
-      return {
-        formData: {
-          vendor: 'Rim Jobs Auto Parts',
-          trans: '',
-          cc: '',
-          name: '',
-          exp: '',
-          amount: ''
-        },
-        message: '',
-        errormessage: null,
-        confirmation: null,
+  data() {
+    return {
+      formData: {
+        vendor: '',
+        trans: '',
+        cc: '',
+        name: '',
+        exp: '',
+        amount: ''
+      },
+      month: 1,
+      year: 2021,
+      message: '',
+      errormessage: null,
+      confirmation: null,
+    }
+  },
+  methods: {
+    submitForm() {
+      // set the vendor to our name later
+      this.formData.vendor = 'STOREFRONT';
+      // generate a random order number
+      this.formData.trans = this.orderNumber();
+      // get the total cost from the local storage
+      this.formData.amount = localStorage.getItem('totalCost');
+      // get the expiration date
+      this.formData.exp = this.month + '/' + this.year;
 
-      }
-    },
-    methods: {
-      submitForm() {
-        fetch('/api/creditcardauth', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(this.formData)
-        })
+      fetch('/api/creditcardauth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.formData)
+      })
         .then(response => {
           if (response.status === 500) {
-              return response.json().then(data => {
+            return response.json().then(data => {
               this.message = `Unable to authorize payment with the following errors:`
               this.errormessage = data.errors
             })
@@ -42,19 +52,16 @@ const app = Vue.createApp({
         .catch(error => {
           console.error(error)
         });
-      },
-      orderNumber() {
-        let now = Date.now().toString()
-        // pad with extra random digit
-        now += now + Math.floor(Math.random() * 10)
-        // format
-        console.log([now.slice(0, 4), now.slice(4, 10), now.slice(10, 14)].join('-'));
-        return  [now.slice(0, 4), now.slice(4, 10), now.slice(10, 14)].join('-');
-      },
-      Authorize() {
-        this.trans = this.orderNumber();
-      }
-    }
-  });
-  
-  app.mount('#ccauth');
+    },
+    orderNumber() {
+      let now = Date.now().toString()
+      // pad with extra random digit
+      now += now + Math.floor(Math.random() * 10)
+      // format
+      console.log([now.slice(0, 4), now.slice(4, 10), now.slice(10, 14)].join('-'));
+      return [now.slice(0, 4), now.slice(4, 10), now.slice(10, 14)].join('-');
+    },
+  }
+});
+
+app.mount('#ccauth');
